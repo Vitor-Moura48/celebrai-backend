86be9509490e2b52b0483bae5b4feb52fff37f3c
Ott-on <ottonvinicius1004@gmail.com>
2025-11-26 15:57:37 -0300

    feat: Criando Tabela PedidoKit

---
+++ Celebrai/src/Backend/Celebrai.Domain/Entities/PedidoKit.cs
@@ -0,0 +1,15 @@
+﻿namespace Celebrai.Domain.Entities;
+
+public class PedidoKit
+{
+    public int IdPedido { get; set; }
+    public int IdKit { get; set; }
+    public int Quantidade { get; set; }
+    public decimal Preco { get; set; }
+    public string? Avaliacao { get; set; }
+    public int? Nota { get; set; }
+    public Pedido Pedido { get; set; } = default!;
+    public Kit Kit { get; set; } = default!;
+
+}
+
