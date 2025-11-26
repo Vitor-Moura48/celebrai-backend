4b36d521978c367f0af6821b8d5e82f81dad2b7e
Ott-on <ottonvinicius1004@gmail.com>
2025-11-26 06:44:53 -0300

    fix: Ajustando Produto colocando o atributo PrecoUnitario

--- Celebrai/src/Backend/Celebrai.Domain/Entities/Produto.cs
+++ Celebrai/src/Backend/Celebrai.Domain/Entities/Produto.cs
@@ -5,6 +5,7 @@ public class Produto
     public int IdProduto { get; set; }
     public int IdSubcategoria { get; set; }
     public string Nome { get; set; } = string.Empty;
+    public decimal PrecoUnitario { get; set; }
     public string Descricao { get; set; } = string.Empty;
     public string ImagemUrl { get; set; } = string.Empty;
     public string ImagemPublicId { get; set; } = string.Empty;
---
+++ Celebrai/src/Backend/Celebrai.Infrastructure/Migrations/Versions/M202511260001AdicionarColunaPrecoUnitario.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace Celebrai.Infrastructure.Migrations.Versions;
+
+[Migration(202511260001, "Adição da Coluna PrecoUnitario")]
+[Profile("Celebrai")]
+public class M202511260001AdicionarColunaPrecoUnitario : Migration
+{
+    public override void Up()
+    {
+        Alter.Table("Produto")
+            .AddColumn("PrecoUnitario").AsCurrency().NotNullable();
+    }
+
+    public override void Down()
+    {
+        if (Schema.Table("Produto").Column("PrecoUnitario").Exists())
+        {
+            Delete.Column("PrecoUnitario").FromTable("Produto");
+        }
+    }
+}
