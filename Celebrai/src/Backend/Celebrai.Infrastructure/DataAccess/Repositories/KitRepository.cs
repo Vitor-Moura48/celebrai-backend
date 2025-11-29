704b42e3b0bf04f55918f7635fbaeea1eca3f730
Ott-on <ottonvinicius1004@gmail.com>
2025-11-29 20:26:35 -0300

    feat: adicionando o repository do Kit

---
+++ Celebrai/src/Backend/Celebrai.Infrastructure/DataAccess/Repositories/KitRepository.cs
@@ -0,0 +1,19 @@
+﻿using Celebrai.Domain.Dtos;
+using Celebrai.Domain.Entities;
+using Celebrai.Domain.Repositories.Kit;
+using Microsoft.EntityFrameworkCore;
+
+namespace Celebrai.Infrastructure.DataAccess.Repositories;
+
+public class KitRepository : IKitWriteOnlyRepository
+{
+    private readonly CelebraiDbContext _context;
+
+    public KitRepository(CelebraiDbContext context) => _context = context;
+
+    public async Task Add(Kit kit, IEnumerable<ProdutoKit> produtosDoKit)
+    {
+        await _context.Kit.AddAsync(kit);
+        await _context.ProdutoKit.AddRangeAsync(produtosDoKit);
+    }
+}
