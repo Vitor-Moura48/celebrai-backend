d05a5a80a7a1f7114bf7e058b3d8907facb354b3
Ott-on <ottonvinicius1004@gmail.com>
2025-11-29 20:29:54 -0300

    feat: Validador do RegisterKit

---
+++ Celebrai/src/Backend/Celebrai.Application/UseCases/Kit/Register/RegisterKitValidator.cs
@@ -0,0 +1,22 @@
+﻿using Celebrai.Communication.Requests.Kit;
+using Celebrai.Domain.Repositories.Produto;
+using FluentValidation;
+
+namespace Celebrai.Application.UseCases.Kit.Register;
+
+public class RegisterKitValidator : AbstractValidator<RequestKitJson>
+{
+    public RegisterKitValidator(IProdutoReadOnlyRepository produtoReadOnlyRepository)
+    {
+        RuleFor(kit => kit.Nome).NotEmpty().WithMessage("O nome é obrigatório.");
+        RuleFor(kit => kit.Descricao).NotEmpty().WithMessage("A descrição é obrigatória.");
+        RuleFor(kit => kit.KitPreco).GreaterThan(0).WithMessage("O preço do kit deve ser maior que zero.");
+        RuleForEach(kit => kit.ProdutosIds)
+                    .MustAsync(async (produtoId, cancellation) =>
+                    {
+                       var produto = await produtoReadOnlyRepository.GetProdutoByIdentifier(produtoId);
+                       return produto != null; 
+                    })
+                    .WithMessage("O produto com o ID {PropertyValue} não foi encontrado.");
+    }
+}
