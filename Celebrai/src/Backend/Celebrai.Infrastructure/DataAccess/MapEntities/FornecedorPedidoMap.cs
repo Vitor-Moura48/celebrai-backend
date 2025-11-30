4d6fd50a697c3441d13697f673a8e133b5524677
Rafaelsn11 <faelsantana001@gmail.com>
2025-11-29 23:40:06 -0300

    feat: criação da tabela FornecedorPedido

---
+++ Celebrai/src/Backend/Celebrai.Infrastructure/DataAccess/MapEntities/FornecedorPedidoMap.cs
@@ -0,0 +1,29 @@
+﻿using Celebrai.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Celebrai.Infrastructure.DataAccess.MapEntities;
+
+public class FornecedorPedidoMap : BaseMap<FornecedorPedido>
+{
+    public FornecedorPedidoMap() : base("FornecedorPedidoMap")
+    {
+    }
+
+    override public void Configure(EntityTypeBuilder<FornecedorPedido> builder)
+    {
+        base.Configure(builder);
+
+        builder.HasKey(x => new { x.IdFornecedor, x.IdPedido });
+
+        builder.HasOne(x => x.Fornecedor)
+               .WithMany()
+               .HasForeignKey(x => x.IdFornecedor)
+               .HasConstraintName("FK_FornecedorPedido_Fornecedor");
+
+        builder.HasOne(x => x.Pedido)
+               .WithMany()
+               .HasForeignKey(x => x.IdPedido)
+               .HasConstraintName("FK_FornecedorPedido_Pedido");
+    }
+}
