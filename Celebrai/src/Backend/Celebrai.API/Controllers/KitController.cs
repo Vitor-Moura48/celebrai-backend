364069ba2235daa8101c8451decc493ab984f9e7
Ott-on <ottonvinicius1004@gmail.com>
2025-11-30 16:10:17 -0300

    refactor: GetList e GetById do Repository

--- Celebrai/src/Backend/Celebrai.API/Controllers/KitController.cs
+++ Celebrai/src/Backend/Celebrai.API/Controllers/KitController.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure;
 using Celebrai.API.Attributes;
+using Celebrai.Application.UseCases.Kit.GetById;
 using Celebrai.Application.UseCases.Kit.Register;
+using Celebrai.Application.UseCases.Produto.GetList;
 using Celebrai.Communication.Requests.Kit;
 using Celebrai.Communication.Responses;
 using Celebrai.Communication.Responses.Kit;
@@ -23,6 +26,30 @@ namespace Celebrai.API.Controllers
         {
             var result = await useCase.Execute(request);
             return Created(string.Empty, result);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IList<ResponseKitJson>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetKitsList(
+            [FromServices] IGetListKitUseCase useCase,
+            [FromQuery] int? page)
+        {
+            var result = await useCase.Execute(page);
+            
+            return Ok(result);   
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(typeof(ResponseKitJson), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetKitById(
+            [FromServices] IGetKitByIdUseCase useCase,
+            [FromRoute] int id)
+        {
+            var result = await useCase.Execute(id);
+
+            return Ok(result);   
         }
     }
 }
--- Celebrai/src/Backend/Celebrai.Infrastructure/DataAccess/Repositories/KitRepository.cs
+++ Celebrai/src/Backend/Celebrai.Infrastructure/DataAccess/Repositories/KitRepository.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Celebrai.Infrastructure.DataAccess.Repositories;
 
-public class KitRepository : IKitWriteOnlyRepository
+public class KitRepository : IKitWriteOnlyRepository, IKitReadOnlyRepository
 {
     private readonly CelebraiDbContext _context;
 
@@ -15,5 +15,35 @@ public class KitRepository : IKitWriteOnlyRepository
     {
         await _context.Kit.AddAsync(kit);
         await _context.ProdutoKit.AddRangeAsync(produtosDoKit);
+    }
+
+    public async Task<Kit?> GetByIdAsync(int id)
+    {
+        return await _context.Kit
+            .AsNoTracking()
+            .Include(kit => kit.ProdutosKit)
+                .ThenInclude(produtoKit => produtoKit.Produto)
+                    .ThenInclude(produto => produto.SubCategoria)
+            .FirstOrDefaultAsync(kit => kit.IdKit == id);
+    }
+
+    public async Task<IList<Kit>> GetKitsList(int? page)
+    {
+        const int pageSize = 15;
+
+        var query = _context.Kit.AsQueryable();
+
+        query = query.OrderBy(kit => kit.IdKit);
+
+        if (page is not null && page > 0)
+            query = query.Skip(((int)page - 1) * pageSize);
+
+        return await query
+            .AsNoTracking()
+            .Take(pageSize)
+            .Include(kit => kit.ProdutosKit)
+            .ThenInclude(produtosKit => produtosKit.Produto)
+            .ThenInclude(produto => produto.SubCategoria)
+            .ToListAsync();
     }
 }
